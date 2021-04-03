cc02552841f3046b3c9173865ff3f41e2b952d04
RP-72 <59957244+RP-72@users.noreply.github.com>
2021-04-03 18:48:02 +0530

    Final "CREATE TABLE" queries added.

--- starter_queries.sql
+++ starter_queries.sql
@@ -23,10 +23,30 @@ CREATE TABLE movie(
     title varchar(40) NOT NULL,
     production_cost int,
     genre varchar(10),
-    rating int CHECK(rating>0 and rating <=10),
+    rating float CHECK(rating>0 and rating <=10),
     release_date Date,
     platform varchar(20),
+    director varchar(10),
+    FOREIGN KEY (director) REFERENCES Person(person_id),
     FOREIGN KEY (platform) REFERENCES ott_platform(platform_name)
+);
+
+CREATE TABLE db_user(
+    email varchar(50) PRIMARY KEY,
+    username varchar(20) UNIQUE,
+    firstname varchar(20) NOT NULL, 
+    lastname varchar(20) NOT NULL
+);
+
+CREATE TABLE review(
+    review_id varchar(10) PRIMARY KEY,
+    posted_on Date,
+    content varchar(1000),
+    up_votes int,
+    movie_id varchar(10),
+    email varchar(10),
+    FOREIGN KEY (movie_id) REFERENCES movie(movie_id),
+    FOREIGN KEY (email) REFERENCES db_user(email)
 );
 
 CREATE TABLE movie_produced_by(
@@ -74,3 +94,15 @@ CREATE TABLE show_cast(
     FOREIGN KEY (show_id) REFERENCES tv_show(show_id),
     FOREIGN KEY (person_id) REFERENCES person(person_id)
 );
+
+-- DROP TABLE show_cast;
+-- DROP TABLE show_produced_by;
+-- DROP TABLE tv_show;
+-- DROP TABLE movie_cast;
+-- DROP TABLE movie_produced_by;
+-- DROP TABLE review;
+-- DROP TABLE user;
+-- DROP TABLE movie;
+-- DROP TABLE ott_platform;
+-- DROP TABLE Production_company;
+-- DROP TABLE Person;
