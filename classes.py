1b5e7a74515baddd9a973e045a6b01e28bba8a6a
Hirmay Sandesara <56473003+Hirmay@users.noreply.github.com>
2021-04-16 16:51:36 +0530

    Fixed a bug and added admin features

--- classes.py
+++ classes.py
@@ -1,20 +1,18 @@
 # Creating search classes in this python file
 from wtforms import Form, StringField, SelectField
 class Search_M_W(Form):
-    choices = [('Movie', 'Movie'),
-               ('Web-Series', 'Web-Series'), ('Celebrity', 'Celebrity'),
-              ('Director', 'Director')]
+    choices = [('Movie', 'Movie'),('Celebrity', 'Celebrity')]
     select = SelectField('Search:', choices=choices)
     search = StringField('')
 
 class ADD_M_W(Form):
     choices = [('Movie', 'Movie'),
                ('Web-Series', 'Web-Series')]
-    select = SelectField('Add a Movie or Web-Series:', choices=choices)
+    select = SelectField('Add a Movie or Celebrity:', choices=choices)
     search = StringField('')  
     
 class DELETE_M_W(Form):
     choices = [('Movie', 'Movie'),
-               ('Web-Series', 'Web-Series')]
-    select = SelectField('Delete a Movie or Web-Series:', choices=choices)
-    search = StringField('')  
+               ('Celebrity', 'Celebrity')]
+    select = SelectField('Delete a Movie or Celebrity:', choices=choices)
+    search = StringField('')
