0671031d5389ecd149f31c1170f05680e88ef10a
Hirmay Sandesara <56473003+Hirmay@users.noreply.github.com>
2021-04-16 11:35:26 +0530

    Update db_triggers.sql

--- db_triggers.sql
+++ db_triggers.sql
@@ -10,7 +10,6 @@ begin
   delete from movie_cast where movie_cast.movie_id = OLD.movie_id;
   delete from movie_review where movie_review.movie_id = OLD.movie_id;
   update ott_platform set total = total -1 where platform_name = OLD.platform;
-  --delete from movie where movie.movie_id = OLD.movie_id;
   RETURN OLD;
 end;
 $$;
@@ -24,49 +23,46 @@ delete from movie where movie_id = '1';
 
 ------------------trigger to delete shows------------------ 
 
-CREATE OR REPLACE FUNCTION delete_shows() RETURNS TRIGGER 
-LANGUAGE plpgsql
-as $$
-DECLARE
-begin
-  delete from show_genre where show_genre.show_id = OLD.show_id;
-  delete from show_produced_by where show_produced_by.show_id = OLD.show_id;
-  delete from show_cast where show_cast.show_id = OLD.show_id;
-  delete from show_review where show_review.show_id = OLD.show_id;
-  update ott_platform set total = total -1 where platform_name = OLD.platform;
-  --delete from show where show.show_id = OLD.show_id;
-  RETURN OLD;
-end;
-$$;
+-- CREATE OR REPLACE FUNCTION delete_shows() RETURNS TRIGGER 
+-- LANGUAGE plpgsql
+-- as $$
+-- DECLARE
+-- begin
+--   delete from show_genre where show_genre.show_id = OLD.show_id;
+--   delete from show_produced_by where show_produced_by.show_id = OLD.show_id;
+--   delete from show_cast where show_cast.show_id = OLD.show_id;
+--   delete from show_review where show_review.show_id = OLD.show_id;
+--   update ott_platform set total = total -1 where platform_name = OLD.platform;
+--   RETURN OLD;
+-- end;
+-- $$;
 
-CREATE TRIGGER show_delete 
-BEFORE DELETE ON tv_show
-FOR EACH ROW 
-EXECUTE PROCEDURE delete_shows();
+-- CREATE TRIGGER show_delete
+-- BEFORE DELETE ON tv_show
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE delete_shows();
 
-delete from show where show_id = '1';
+-- delete from show where show_id = '1';
 
 ---------------------------trigger to delete celebs-----------------------------------
 
-CREATE OR REPLACE FUNCTION delete_celeb() RETURNS TRIGGER 
+CREATE OR REPLACE FUNCTION delete_celeb() RETURNS TRIGGER
 LANGUAGE plpgsql
 as $$
 DECLARE
 begin
-  --delete from movie_produced_by where movie_produced_by.person_id = OLD.person_id;
   delete from movie_cast where movie_cast.person_id = OLD.person_id;
-  delete from show_cast where show_cast.person_id = OLD.person_id;
+  --delete from show_cast where show_cast.person_id = OLD.person_id;
   update movie set director = NULL where director = OLD.person_id;
   RETURN OLD;
 end;
 $$;
 
-CREATE TRIGGER celeb_delete 
+CREATE TRIGGER celeb_delete
 BEFORE DELETE ON celebrity
-FOR EACH ROW 
+FOR EACH ROW
 EXECUTE PROCEDURE delete_celeb();
 
---to test the above trigger: 
 delete from celebrity where person_id = '20';
 
 -----------if review contains an abusive word trigger will fire-------------
@@ -76,12 +72,27 @@ LANGUAGE plpgsql
 AS $$
 DECLARE
 	r_abusive record;
+	r_user record;
 begin
 	for r_abusive in select words from abusive_words loop
 	  if r_abusive.words in (select unnest(string_to_array(NEW.content,' '))) then
-		-- raise notice '%', r_abusive.words;
-		UPDATE db_user SET warning = warning + 1 where 
-		username = NEW.username;
+		UPDATE db_user SET warning = warning + 1 where username = NEW.username;
+		raise notice '%', NEW.username;
+		for r_user in select * from db_user loop
+		  if r_user.username = NEW.username then
+		    r_user.warning:=r_user.warning + 1;
+		  	raise notice '% and %', r_user.username, r_user.warning;
+		  end if; 
+		end loop;
+		-- for r_user in select * from db_user loop
+		--   if r_user.username = NEW.username then 
+		-- 	if r_user.warning = 3 then 
+		-- 	  insert into blocked_user(email) values(r_user.email);
+		-- 	  delete from db_user where username = r_user.username;
+		-- 	  raise exception using message = 'Your account has been blocked for using abusive language';
+		-- 	end if;
+		--   end if;
+		-- end loop;
 		raise exception using message = 'Abusive language detected';
 		RETURN OLD;
 	  end if;
@@ -176,18 +187,66 @@ BEFORE DELETE ON movie_produced_by
 FOR EACH ROW 
 EXECUTE PROCEDURE delete_mov_prod();
 
+---------------------------trigger to delete user -----------------------------------
 
+CREATE OR REPLACE FUNCTION delete_user() RETURNS TRIGGER 
+LANGUAGE plpgsql
+as $$
+DECLARE
+begin
+  delete from movie_review where movie_review.username = OLD.username; 
+  --delete from show_review where show_review.username = OLD.username; 
+--   delete from movie_cast where movie_cast.person_id = OLD.person_id;
+--   delete from show_cast where show_cast.person_id = OLD.person_id;
+--   update movie set director = NULL where director = OLD.person_id;
+  RETURN OLD;
+end;
+$$;
+
+CREATE TRIGGER user_delete 
+BEFORE DELETE ON db_user
+FOR EACH ROW 
+EXECUTE PROCEDURE delete_user();
+
+
+---------------------------trigger to delete user -----------------------------------
+
+CREATE OR REPLACE FUNCTION check_if_user_blocked() RETURNS TRIGGER 
+LANGUAGE plpgsql
+as $$
+DECLARE
+	r_blocked_user record;
+begin
+  for r_blocked_user in select * from blocked_user loop 
+	  if r_blocked_user.email = NEW.email then
+		raise exception using message = 'This email has been blocked. Contact the admin if you think this is a mistake.';
+		return OLD;
+	  end if;
+  end loop;
+  RETURN NEW;
+end;
+$$;
+
+CREATE TRIGGER if_user_blocked 
+BEFORE INSERT ON db_user
+FOR EACH ROW 
+EXECUTE PROCEDURE check_if_user_blocked();
+ 
 -----------------------------------------------------------
 DELETE FROM db_user;
 INSERT INTO db_user(email, username, date_of_birth, firstname, lastname, hash,warning) 
 VALUES('ro@gmail.com','ro','2000-01-01','r','p','temphash',0);
 
-INSERT INTO movie(movie_id, title, platform) VALUES('1111','Test','Netflix');
 
+-- INSERT INTO db_user(email, username, date_of_birth, firstname, lastname, hash,warning) 
+-- VALUES('rp@gmail.com','rp','2000-01-01','r','p','temphash',0);
+
+
+INSERT INTO movie(movie_id, title, platform) VALUES('1111','Test','Netflix');
 
 DELETE FROM movie_review;
 INSERT INTO movie_review(review_id, posted_on, content, up_votes, movie_id, username)
 VALUES ('mr0001','2000-01-01','this fuck movie is good', 0, '1','ro');
 
 INSERT INTO movie_review(review_id, posted_on, content, up_votes, movie_id, username)
-VALUES ('mr0002','2000-01-01','this movie is sooo good', 0, '2','ro');
+VALUES ('mr0008','2000-01-01','this movie is sooo good', 0, '2','ro');
